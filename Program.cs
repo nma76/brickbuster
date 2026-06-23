c9c732a2d3b1891ac8b70a2b88c5531fc1b5a53a
Jonas Lindau <jonas@jonika.nu>
2026-06-23 11:21:15 +0200

    Replaced top-level statement with Main

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿using var game = new brickbuster.Game1();
-game.Run();
+﻿using System;
+using System.Linq;
+
+internal static class Program
+{
+    public static void Main(string[] args)
+    {
+        bool editorMode = args.Contains("--editor");
+
+        using var game = new brickbuster.Game1();
+        game.Run();
+    }
+}
